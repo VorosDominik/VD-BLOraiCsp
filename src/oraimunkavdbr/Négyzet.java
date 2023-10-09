ef8cbecd520262691545846eb065f99e34418c1e
ASDleves <ricsi20020302@gmail.com>
2023-10-09 08:31:31 +0200

    negyzet elkeszítese

--- src/oraimunkavdbr/Négyzet.java
+++ src/oraimunkavdbr/Négyzet.java
@@ -1,13 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package oraimunkavdbr;
 
-/**
- *
- * @author bella.richard
- */
 public class Négyzet {
+    
+    private int kerulet;
+    private int terulet;
+    private int xkoordinata;
+    private int ykoordinata;
+
+    public void setKerulet(int kerulet) {
+        this.kerulet = kerulet;
+    }
+
+    public void setTerulet(int terulet) {
+        this.terulet = terulet;
+    }
+
+    public void setXkoordinata(int xkoordinata) {
+        this.xkoordinata = xkoordinata;
+    }
+
+    public void setYkoordinata(int ykoordinata) {
+        this.ykoordinata = ykoordinata;
+    }
+
+    public int getKerulet() {
+        return kerulet;
+    }
+
+    public int getTerulet() {
+        return terulet;
+    }
+
+    public int getXkoordinata() {
+        return xkoordinata;
+    }
+
+    public int getYkoordinata() {
+        return ykoordinata;
+    }
+
+    @Override
+    public String toString() {
+        return "N\u00e9gyzet{" + "kerulet=" + kerulet + ", terulet=" + terulet + ", xkoordinata=" + xkoordinata + ", ykoordinata=" + ykoordinata + '}';
+    }
     
 }
---
+++ src/oraimunkavdbr/Program.java
@@ -0,0 +1,10 @@
+
+package oraimunkavdbr;
+
+public class Program {
+
+    public static void main(String[] args) {
+    System.out.println(new Négyzet().toString());
+    }
+    
+}
