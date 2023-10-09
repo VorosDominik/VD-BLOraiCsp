b23d537e1c9a1a714c9fb979e23467739c016ecc
ASDleves <ricsi20020302@gmail.com>
2023-10-09 09:52:24 +0200

    összehasonlítás osztályban

--- src/oraimunkavdbr/Négyzet.java
+++ src/oraimunkavdbr/Négyzet.java
@@ -31,7 +31,23 @@ public class Négyzet {
     public void setKoordinata(Koordinata koordinata) {
         this.koordinata = koordinata;
     }
-
+    private boolean osszehasonlit(Négyzet b) {
+        if (this.a == b.getA()) {
+            return true;
+        } else if (this.a < b.getA()) {
+            return false;
+        } else {
+            return false;
+        }
+    }
+    public void osszehasonlitszoveg(Négyzet b) {
+        boolean eredmeny = osszehasonlit(b);
+        if (eredmeny) {
+            System.out.println("A négyzetek egyformák.");
+        } else {
+            System.out.println("A négyzetek nem egyformák.");
+        }
+    }
     public String toString() {
         return "N\u00e9gyzet{" + "terulet=" + a*a + ", a=" + a + ", x koordinata=" + koordinata.getX() + ", y koordinata=" + koordinata.getY() + '}';
     }
