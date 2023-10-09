36dcc0ee134d13cdb450fde4d491068112ba47bd
VorosDominik <medoxidhun@gmail.com>
2023-10-09 08:34:18 +0200

    negyzet feladat

--- src/oraimunkavdbr/Négyzet.java
+++ src/oraimunkavdbr/Négyzet.java
@@ -9,5 +9,49 @@ package oraimunkavdbr;
  * @author bella.richard
  */
 public class Négyzet {
+    private int oldal=4;
+
+   
+    private int x=0;
+    private int y=0;
+     public void setOldal(int oldal) {
+        this.oldal = oldal;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
     
+    
+     public int negyzetterulet(){
+         int terulet=4*oldal;
+     
+        return terulet;
+     
+     
+     };
+    public void kiiras(){
+        System.out.println("a négyzet területe: "+negyzetterulet());
+        System.out.println("x,y koordinátája pedig: "+getX()+" "+getY());
+    
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+    
+
+    @Override
+    public String toString() {
+        return super.toString(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+     
 }
--- src/oraimunkavdbr/Program.java
+++ src/oraimunkavdbr/Program.java
@@ -8,13 +8,13 @@ package oraimunkavdbr;
  *
  * @author bella.richard
  */
-public class OraimunkaVDBR {
+public class Program {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    
     }
     
 }
